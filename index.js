f89ae41b0abc5e24fc434c90ce789e894d374f3b
Mark Laufersweiler <meelyn.pandit@gmail.com>
2024-01-28 18:14:56 -0500

    able to parse milliseconds and string dates

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -21,8 +23,29 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log('api hello req', req.params)
   res.json({greeting: 'hello API'});
 });
+
+app.get('/api/:date?', function (req, res) {
+  console.log('req params', req.params)
+  let params = req.params.date
+  console.log('params', params)
+  // let date_time = new Date(params)
+  if (Number(params)) {
+    let date_time = Number(params)
+    console.log('date time', date_time)
+    let unix = date_time
+    res.json({unix: unix, utc: new Date(unix).toUTCString()})
+  } else {
+    let date_time = params
+    console.log('date time', date_time)
+    let unix = Date.parse(date_time)
+    res.json({unix: unix, utc: new Date(unix).toUTCString()})
+
+  }
+
+})
 
 
 
